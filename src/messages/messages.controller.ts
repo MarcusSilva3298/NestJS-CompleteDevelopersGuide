ff4daad76867901cdd7605f3eae53e4ccd83670a
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-02-09 15:07:25 -0300

    Session 5 halfway through

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { CreateMessageDTO } from './dtos/create-message.dto'
+import { MessagesService } from './messages.service'
 
 @Controller('messages')
 export class MessagesController {
+  constructor(private messagesService = new MessagesService()) {}
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll()
+  }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDTO) {}
+  createMessage(@Body() { content }: CreateMessageDTO) {
+    return this.messagesService.create(content)
+  }
 
   @Get('/:id')
-  getMessage(@Param() param: any) {}
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id)
+  }
 }
