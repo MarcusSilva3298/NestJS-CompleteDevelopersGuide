4cea199f507a2cdb6800eadaf782a6c1410789cb
MarcusSilva3298 <marcussilva3298@gmail.com>
2022-02-14 14:01:46 -0300

    Finished section 6

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { ComputerModule } from './computer/computer.module'
 
 const port = 3000
 const logger = new Logger()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(ComputerModule)
   await app.listen(3000, () => {
     logger.verbose(`Server listenning at port ${port}! 🚀`)
   })
